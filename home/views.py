ac97d33a5a7e667c46ab11f60ba42a66af962f0a
Atul Gupta <atulguptag111@gmail.com>
2024-07-20 11:59:37 +0530

    Added sort by and category filter

--- home/views.py
+++ home/views.py
@@ -1,27 +1,44 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 from django.db.models import Q
 
 # Create your views here.
 
 
 def index(request):
-    context = {'products': Product.objects.all()}
+    query = Product.objects.all()
+    categories = Category.objects.all()
+    selected_sort = request.GET.get('sort')
+    selected_category = request.GET.get('category')
+
+    if selected_category:
+        query = query.filter(category__category_name=selected_category)
+
+    if selected_sort:
+        if selected_sort == 'priceAsc':
+            query = query.order_by('price')
+
+        elif selected_sort == 'priceDesc':
+            query = query.order_by('-price')
+
+    context = {
+        'products': query,
+        'categories': categories,
+        'selected_category': selected_category,
+        'selected_sort': selected_sort,
+    }
     return render(request, 'home/index.html', context)
 
 
 def product_search(request):
     query = request.GET.get('q', '')
-    products = []
 
     if query:
-        # Search for products that contain the query string in their name or description
-        products = Product.objects.filter(
-            Q(name__icontains=query) | Q(description__icontains=query))
+        # Search for products that contain the query string in their product_name field
+        products = Product.objects.filter(Q(product_name__icontains=query) | Q(
+            product_name__istartswith=query))
+    else:
+        products = Product.objects.none()
 
-    context = {
-        'query': query,
-        'products': products
-    }
-
-    return render(request, 'home/search.html', context) 
+    context = {'query': query, 'products': products}
+    return render(request, 'home/search.html', context)
