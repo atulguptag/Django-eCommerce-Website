7567aca21ef3849726d7f0b55043048bd1a3367e
Atul Gupta <atulguptag111@gmail.com>
2024-07-24 15:17:18 +0530

    Added forgot-password login and registration directory and related files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
-from accounts.views import login_page, register_page, activate_email_account, add_to_cart, cart, remove_cart, remove_coupon, success, user_logout
+from accounts.views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', login_page, name="login"),
     path('register/', register_page, name="register"),
     path('logout/', user_logout, name='logout'),
     path('activate/<email_token>/', activate_email_account, name="activate_email"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html'), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+        
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('cart/', cart, name="cart"),
     path('add-to-cart/<uid>/', add_to_cart, name="add_to_cart"),
     path('remove-cart/<uid>/', remove_cart, name="remove_cart"),
