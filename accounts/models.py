15be8a2ebfef960341b5c6e5c613855c6ad4d5e7
Atul Gupta <atulguptag111@gmail.com>
2024-08-12 00:31:39 +0530

    Removed send email_token code

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from base.models import BaseModel
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-import uuid
-from base.emails import send_account_activation_email
 from products.models import Product, ColorVariant, SizeVariant, Coupon
 
 # Create your models here.
@@ -21,18 +17,6 @@ class Profile(BaseModel):
     def get_cart_count(self):
         return CartItem.objects.filter(cart__is_paid=False, cart__user=self.user).count()
 
-
-@receiver(post_save, sender=User)
-def send_email_token(sender, instance, created, **kwargs):
-    try:
-        if created:
-            email_token = str(uuid.uuid4())
-            Profile.objects.create(user=instance, email_token=email_token)
-            email = instance.email
-            send_account_activation_email(email, email_token)
-
-    except Exception as e:
-        print(e)
 
 
 class Cart(BaseModel):
