a1620eba42456fe9c440577809d71dc54b1b75ef
Atul Gupta <atulguptag111@gmail.com>
2024-08-12 13:18:54 +0530

    Added Related product items in product page.

--- products/views.py
+++ products/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from products.models import Product
+import random
 
 def get_product(request, slug):
-    try:
-        product = get_object_or_404(Product, slug=slug)
-        sorted_size_variants = product.size_variant.all().order_by('size_name')
-        context = {
-            'product': product,
-            'sorted_size_variants': sorted_size_variants
-        }
+    product = get_object_or_404(Product, slug=slug)
+    sorted_size_variants = product.size_variant.all().order_by('size_name')
+    related_products = list(product.category.products.filter(parent=None).exclude(uid=product.uid))
 
-        if request.GET.get('size'):
-            size = request.GET.get('size')
-            price = product.get_product_price_by_size(size)
-            context['selected_size'] = size
-            context['updated_price'] = price
+    print(len(related_products))
+    
+    if len(related_products) >= 4:
+        related_products = random.sample(related_products, 4)
 
-        return render(request, 'product/product.html', context=context)
-    except Exception as e:
-        print(e)
+    context = {
+        'product': product,
+        'sorted_size_variants': sorted_size_variants,
+        'related_products': related_products
+    }
+
+    if request.GET.get('size'):
+        size = request.GET.get('size')
+        price = product.get_product_price_by_size(size)
+        context['selected_size'] = size
+        context['updated_price'] = price
+
+    return render(request, 'product/product.html', context=context)
