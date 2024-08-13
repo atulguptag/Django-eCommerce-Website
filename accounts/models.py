14de1062e817cd328b300720040c81eda6faed88
Atul Gupta <atulguptag111@gmail.com>
2024-08-13 23:45:11 +0530

    Added product.price * self.quantity

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from base.models import BaseModel
 from products.models import Product, ColorVariant, SizeVariant, Coupon
+from home.models import ShippingAddress
 
 # Create your models here.
 
@@ -13,6 +14,11 @@ class Profile(BaseModel):
     is_email_verified = models.BooleanField(default=False)
     email_token = models.CharField(max_length=100, null=True, blank=True)
     profile_image = models.ImageField(upload_to='profile', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE, related_name="shipping_address", null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
 
     def get_cart_count(self):
         return CartItem.objects.filter(cart__is_paid=False, cart__user=self.user).count()
@@ -52,9 +58,10 @@ class CartItem(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     color_variant = models.ForeignKey(ColorVariant, on_delete=models.SET_NULL, null=True, blank=True)
     size_variant = models.ForeignKey(SizeVariant, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=1)
 
     def get_product_price(self):
-        price = self.product.price
+        price = self.product.price * self.quantity
 
         if self.color_variant:
             price += self.color_variant.price
