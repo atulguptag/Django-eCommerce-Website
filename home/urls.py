cb918d3b8d2c7421ef9630a88dc2b07ff31b7c23
Atul Gupta <atulguptag111@gmail.com>
2024-08-13 23:48:54 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from home.views import index, product_search, contact, about
+from home.views import *
 
 urlpatterns = [
     path('', index, name="index"),
     path('search/', product_search, name='product_search'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
+    path('shipping-address/', update_shipping_address, name='shipping-address'),
+    path('terms-and-conditions/', terms_and_conditions, name='terms-and-conditions'),
+    path('privacy-policy/', privacy_policy, name='privacy-policy'),
 ]
