7cdba25cd941a45b23fc452e0f8dedc41c043574
Atul Gupta <atulguptag111@gmail.com>
2024-08-13 23:48:46 +0530

    Added ShippingAddress and ShippingAddressForm model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
+from base.models import BaseModel
+from django.urls import reverse
 from django.db import models
+from django import forms
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+class ShippingAddress(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    street_number = models.CharField(max_length=10)
+    zip_code = models.CharField(max_length=30)
+    city = models.CharField(max_length=50)
+    country = CountryField()
+    phone = models.CharField(max_length=30)
+    current_address = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Shipping address for {self.user.username}: {self.street} {self.street_number}, {self.city}'
+
+    def get_absolute_url(self):
+        return reverse('shipping-address')
+
+class ShippingAddressForm(forms.ModelForm):
+    save_address = forms.BooleanField(required=False, label='Save the billing addres')
+
+    class Meta:
+        model = ShippingAddress
+        fields = [
+            'first_name',
+            'last_name',
+            'street',
+            'street_number',
+            'zip_code',
+            'city',
+            'country',
+            'phone'
+        ]
