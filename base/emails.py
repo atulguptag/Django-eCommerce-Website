6ce73cdf62259633a55f1a14bb402ff7b2c0ee57
Atul Gupta <atulguptag111@gmail.com>
2024-08-13 23:47:19 +0530

    Update emails.py

--- base/emails.py
+++ base/emails.py
@@ -5,5 +5,5 @@ from django.conf import settings
 def send_account_activation_email(email, email_token):
     subject = "Your account needs to be verified"
     email_from = settings.DEFAULT_FROM_EMAIL
-    message = f'Hi, click on the link to activate your account http://127.0.0.1:8000/accounts/activate/{email_token}'
+    message = f'Hi, please verify your account.\nClick on the link to activate your account http://127.0.0.1:8000/accounts/activate/{email_token}'    
     send_mail(subject, message, email_from, [email])
