0b1b644c5bfdb35aa25df9462849724241c6e65a
Atul Gupta <atulguptag111@gmail.com>
2024-08-13 23:48:19 +0530

    Update urls.py

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('product/', include('products.urls')),
     path('accounts/', include('accounts.urls')),
-    path("accounts/", include("allauth.urls")), #added this line
+    path("accounts/", include("allauth.urls")),
 ]
 
 
