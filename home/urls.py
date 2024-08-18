11539cf2572a5b5cd094ab274a39f41eb59a8114
Atul Gupta <atulguptag111@gmail.com>
2024-08-19 01:13:39 +0530

    Removed shipping-address from home url

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('search/', product_search, name='product_search'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
-    path('shipping-address/', update_shipping_address, name='shipping-address'),
     path('terms-and-conditions/', terms_and_conditions, name='terms-and-conditions'),
     path('privacy-policy/', privacy_policy, name='privacy-policy'),
 ]
