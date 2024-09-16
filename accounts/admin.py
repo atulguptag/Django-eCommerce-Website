273da5964f1608394b874f2fac1e82a9a0850573
Atul Gupta <atulguptag111@gmail.com>
2024-09-16 13:37:18 +0530

    Added Order and OrdeItem in admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Cart, CartItem
+from .models import Profile, Cart, CartItem, Order, OrderItem
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
