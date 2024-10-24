b3ccc2c0b9532cc8c82b5773ebddbb1906717a4e
Atul Gupta <atulguptag111@gmail.com>
2024-10-24 13:08:10 +0530

    Fixed pagination error

--- home/views.py
+++ home/views.py
@@ -17,32 +17,31 @@ def index(request):
     selected_sort = request.GET.get('sort')
     selected_category = request.GET.get('category')
 
-    # Pagination Configuration
-    page = request.GET.get('page', 1)
-    paginator = Paginator(query, 20)
-
-    try:
-        query = paginator.page(page)
-    except PageNotAnInteger:
-        query = paginator.page(1)
-    except EmptyPage:
-        query = paginator.page(paginator.num_pages)
-
     if selected_category:
         query = query.filter(category__category_name=selected_category)
 
     if selected_sort:
         if selected_sort == 'newest':
             query = query.filter(newest_product=True).order_by('category_id')
-
         elif selected_sort == 'priceAsc':
             query = query.order_by('price')
-
         elif selected_sort == 'priceDesc':
             query = query.order_by('-price')
 
+    page = request.GET.get('page', 1)
+    paginator = Paginator(query, 20)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    except Exception as e:
+        print(e)
+
     context = {
-        'products': query,
+        'products': products,
         'categories': categories,
         'selected_category': selected_category,
         'selected_sort': selected_sort,
@@ -74,7 +73,6 @@ def contact(request):
             message = request.POST.get('message')
             validate_email(message_email)
 
-
             subject = f"Message from {message_name} {message_lname}"
             email_from = settings.DEFAULT_FROM_EMAIL
 
@@ -86,16 +84,18 @@ def contact(request):
                 fail_silently=False,
             )
 
-            messages.success(request, 'Thank you for your message. We will get back to you soon..')
+            messages.success(
+                request, 'Thank you for your message. We will get back to you soon..')
             return HttpResponseRedirect(request.path_info)
 
         context = {'message_name': message_name}
         return render(request, 'home/contact.html', context)
-    
+
     except Exception:
         messages.error(request, 'Invalid Email Address!')
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 def about(request):
     return render(request, 'home/about.html')
