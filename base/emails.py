8f09f2ecd87bd29ec62b0ff88366558f6f7b55f0
Atul Gupta <atulguptag111@gmail.com>
2024-12-27 15:21:36 +0530

    Added account_activation and password_reset_email template and updated path in urls.py for password reset request.

--- base/emails.py
+++ base/emails.py
@@ -1,9 +1,22 @@
-from django.core.mail import send_mail
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 
 def send_account_activation_email(email, email_token):
     subject = "Your account needs to be verified"
     email_from = settings.DEFAULT_FROM_EMAIL
-    message = f'Hi, please verify your account.\nClick on the link to activate your account http://127.0.0.1:8000/accounts/activate/{email_token}'    
-    send_mail(subject, message, email_from, [email])
+
+    activation_link = f'http://127.0.0.1:8000/accounts/activate/{email_token}'
+
+    html_message = render_to_string(
+        'emails/account_activation.html', {'activation_link': activation_link})
+    plain_message = f'Hi, please verify your account by clicking the link: {activation_link}'
+
+    send_mail(
+        subject,
+        plain_message,
+        email_from,
+        [email],
+        html_message=html_message
+    )
